24ce16b76dbecc6f63608dacf4d13b6ad85314b6
ryuhangyeong <hangyeong.ryu@gmail.com>
2020-12-18 15:33:04 +0900

    react-router-dom 설정 및 hooks helper function

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,34 +18,22 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=4ae0ee7a5c7942bd8df53b32683c4615`,
-        );
-
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-
-      setLoading(false);
-    };
-    fetchData();
-  }, [category]); // category 값이 바뀌면 실행
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=4ae0ee7a5c7942bd8df53b32683c4615`,
+    );
+  }, [category]);
 
   if (loading) {
     return <NewsListBlock>로딩중...</NewsListBlock>;
   }
+  if (!response) return null; // 이 부분이 없으면 오류가 뜬다(매우 중요!)
 
-  if (!articles) return null; // 이 부분이 없으면 오류가 뜬다(매우 중요!)
-
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
